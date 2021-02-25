8a078fc16ddfbbc4d699ab2e2116995fc209f68f
Zefiros13 <filipberic96@gmail.com>
2021-02-25 04:03:48 +0100

    AddedPost

--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PostUploader from "./postUploader";
+import Post from "./post";
 import "../css/Feed.css";
 
 class Feed extends Component {
@@ -7,6 +8,21 @@ class Feed extends Component {
         return ( 
             <div className="feed">
                 <PostUploader/>
+                <Post 
+                 title="Hej, cao!"
+                 profilePicture="https://scontent.fbeg1-1.fna.fbcdn.net/v/t1.0-9/11866310_1163006293728660_6962940529323370142_n.jpg?_nc_cat=107&ccb=3&_nc_sid=174925&_nc_ohc=v9imRJqcb-4AX9F0hEP&_nc_ht=scontent.fbeg1-1.fna&oh=c93477c16d8acd995dfe6446192f5ccc&oe=605D2945"
+                 image="https://i.pinimg.com/originals/c5/58/4a/c5584ae3f6a9aa7f26b276d78af77900.jpg"
+                 username="Filip Beric"
+                />
+
+                <Post 
+                 title="Hej, cao!"
+                 profilePicture="https://scontent.fbeg1-1.fna.fbcdn.net/v/t1.0-9/11866310_1163006293728660_6962940529323370142_n.jpg?_nc_cat=107&ccb=3&_nc_sid=174925&_nc_ohc=v9imRJqcb-4AX9F0hEP&_nc_ht=scontent.fbeg1-1.fna&oh=c93477c16d8acd995dfe6446192f5ccc&oe=605D2945"
+                 image="https://images-na.ssl-images-amazon.com/images/I/41EoST0FdcL._SX331_BO1,204,203,200_.jpg"
+                 username="Filip Beric"
+                />
+
+
             </div>
          );
     }
--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,18 +1,52 @@
 import React, { Component } from 'react'; 
+import { Avatar } from '@material-ui/core';
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import { ExpandMoreOutlined } from "@material-ui/icons";
 import PostOptions from "./postOptions";
+import "../css/Post.css";
 
-class Post extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <React.Fragment>
-                <h1>@Title</h1>
-                <h6>@Creator</h6>
-                <img src="@path"/>
-                <PostOptions/>
-            </React.Fragment>
-         );
-    }
+function Post({profilePicture, image, username, timestamp, title}) {
+    return ( 
+        <div className="post">
+            <div className="post__top">
+                <Avatar src={profilePicture} className="post__avatar"/>
+                <div className="post__topInfo">
+                    <h3>{username}</h3>
+                    <p>{new Date(timestamp?.toDate()).toUTCString()}</p>
+                </div>
+            </div>
+
+            <div className="post__bottom">
+                <p>{title}</p>
+                <div className="post__image">
+                    <img src={image} alt=""/>
+                </div>
+            </div>
+            <div className="post__options">
+                <div className="post__option">
+                    <ThumbUpIcon/>
+                    <p>Like</p>
+                </div>
+                <div className="post__option">
+                    <ChatBubbleOutlineIcon/>
+                    <p>Comment</p>
+                </div>
+                    
+                <div className="post__option">
+                    <NearMeIcon/>
+                    <p>Share</p>
+                </div>
+
+                <div className="post__option">
+                    <AccountCircleIcon/>
+                    <ExpandMoreOutlined/>
+                </div>
+            </div>
+        </div >
+        );
 }
  
 export default Post;
